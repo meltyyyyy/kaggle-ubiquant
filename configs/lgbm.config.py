3fb27773cbf4d899c3307496a6ddbc14a1f6627b
meltyyyyy <takeru.abe.g1@gmail.com>
2022-03-07 14:18:24 +0000

    add lgbm.config.py

---
+++ configs/lgbm.config.py
@@ -0,0 +1,16 @@
+class config:
+
+    SEED = 42
+    FOLD_TO_RUN = 0
+    FOLDS = 10
+    EARLY_STOPPING_ROUNDS = 50
+    VERBOSE = 100
+
+    model_type = 'lgb_regression'
+    EPOCHS = 1000
+    LR = 0.1
+    message='baseline'
+    TARGET = 'target'
+
+    TEST = False
+
