91ff06ff6a62edad447f7253f7b1846d190b2080
meltyyyyy <takeru.abe.g1@gmail.com>
2022-03-08 14:31:19 +0000

    log load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,11 +1,24 @@
 import pandas as pd
 
-from logging import getLogger
+from logging import StreamHandler, DEBUG, Formatter, FileHandler, getLogger
 
+LOG_DIR = '../logs/'
 TRAIN_DATA = '~/ubiquant/data/input/train.csv'
 TEST_DATA = '~/ubiquant/data/input/example_test.csv'
 
 logger = getLogger(__name__)
+log_fmt = Formatter('%(asctime)s %(name)s %(lineno)d [%(levelname)s][%(funcName)s] %(message)s ')
+
+handler = StreamHandler()
+handler.setLevel('INFO')
+handler.setFormatter(log_fmt)
+logger.addHandler(handler)
+
+handler = FileHandler(LOG_DIR + 'load_data.py.log',mode='w')
+handler.setLevel(DEBUG)
+handler.setFormatter(log_fmt)
+logger.setLevel('DEBUG')
+logger.addHandler(handler)
 
 def read_csv(path):
     logger.debug('enter')
