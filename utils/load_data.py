617978e2a79329423354ac9a7781b2324bd09199
meltyyyyy <takeru.abe.g1@gmail.com>
2022-03-12 06:57:32 +0000

    update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -10,13 +10,8 @@ TEST_DATA = '~/ubiquant/data/input/example_test.csv'
 logger = getLogger(__name__)
 log_fmt = Formatter('%(asctime)s %(name)s %(lineno)d [%(levelname)s][%(funcName)s] %(message)s ')
 
-handler = StreamHandler()
-handler.setLevel('INFO')
-handler.setFormatter(log_fmt)
-logger.addHandler(handler)
-
 handler = FileHandler(LOG_DIR + 'load_data.py.log',mode='w')
-handler.setLevel(DEBUG)
+handler.setLevel('DEBUG')
 handler.setFormatter(log_fmt)
 logger.setLevel('DEBUG')
 logger.addHandler(handler)
