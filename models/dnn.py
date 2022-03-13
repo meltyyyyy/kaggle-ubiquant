f2930856c6be4af189df215301d7f9a2c07983d1
meltyyyyy <takeru.abe.g1@gmail.com>
2022-03-13 01:57:51 +0000

    modeling

--- models/dnn.py
+++ models/dnn.py
@@ -1,12 +1,11 @@
+from tensorflow.python.ops import math_ops
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow as tf
+from logging import Formatter, FileHandler, getLogger
 import os, sys
 sys.path.append(os.pardir)
-
 from utils.load_data import load_train_feather
-from logging import Formatter, FileHandler, getLogger
-import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow import keras
-from tensorflow.python.ops import math_ops
 
 LOG_DIR = '../logs/'
 
@@ -67,4 +66,39 @@ def correlation(x, y, axis=-2):
     return tf.constant(1.0, dtype=x.dtype) - corr
 
 
+def get_model():
+    investment_id_inputs = tf.keras.Input((1, ), dtype=tf.uint16)
+    features_inputs = tf.keras.Input((300, ), dtype=tf.float16)
 
+    investment_id_x = investment_id_lookup_layer(investment_id_inputs)
+    investment_id_x = layers.Embedding(
+        investment_ids_size, 32, input_length=1)(investment_id_x)
+    investment_id_x = layers.Reshape((-1, ))(investment_id_x)
+    investment_id_x = layers.Dense(64, activation='swish')(investment_id_x)
+    investment_id_x = layers.Dropout(0.1)(investment_id_x)
+    investment_id_x = layers.Dense(64, activation='swish')(investment_id_x)
+    investment_id_x = layers.Dropout(0.1)(investment_id_x)
+    investment_id_x = layers.Dense(64, activation='swish')(investment_id_x)
+    investment_id_x = layers.Dropout(0.1)(investment_id_x)
+
+    feature_x = layers.Dense(256, activation='swish')(features_inputs)
+    feature_x = layers.Dropout(0.1)(feature_x)
+    feature_x = layers.Dense(256, activation='swish')(feature_x)
+    feature_x = layers.Dropout(0.1)(feature_x)
+    feature_x = layers.Dense(256, activation='swish')(feature_x)
+    feature_x = layers.Dropout(0.1)(feature_x)
+
+    x = layers.Concatenate(axis=1)([investment_id_x, feature_x])
+    x = layers.Dense(512, activation='swish', kernel_regularizer="l2")(x)
+    x = layers.Dropout(0.1)(x)
+    x = layers.Dense(128, activation='swish', kernel_regularizer="l2")(x)
+    x = layers.Dropout(0.1)(x)
+    x = layers.Dense(32, activation='swish', kernel_regularizer="l2")(x)
+    x = layers.Dropout(0.1)(x)
+    output = layers.Dense(1)(x)
+    rmse = keras.metrics.RootMeanSquaredError(name="rmse")
+    model = tf.keras.Model(
+        inputs=[investment_id_inputs, features_inputs], outputs=[output])
+    model.compile(optimizer=tf.optimizers.Adam(0.001), loss='mse',
+                  metrics=['mse', "mae", "mape", rmse, correlation])
+    return model
