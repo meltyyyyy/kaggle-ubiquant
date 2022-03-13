6bb0cfa9c6a4d2cec2743737b56f4c69c5aa461f
meltyyyyy <takeru.abe.g1@gmail.com>
2022-03-13 02:29:12 +0000

    create get_model_summary.py

---
+++ utils/get_model_summary.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+
+def get_model_summary(model: tf.keras.Model) -> str:
+    string_list = []
+    model.summary(line_length=80, print_fn=lambda x: string_list.append(x))
+    return "\n".join(string_list)
